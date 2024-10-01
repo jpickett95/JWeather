b289858f1f7fb6fa3c6ac995d4a8c0d44ca7261d
Jonah Pickett <jpickettpre@gmail.com>
2024-10-01 17:54:15 -0400

    Project setup.

---
+++ JWeather/JWeather/JWeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  JWeatherApp.swift
+//  JWeather
+//
+//  Created by Jonah Pickett on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct JWeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
