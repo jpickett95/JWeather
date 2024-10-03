0e2e8d170945c03bee5e0ecab94d02275a45489f
Jonah Pickett <jpickettpre@gmail.com>
2024-10-03 19:24:17 -0400

    Added image assets and created SkyImages enum

---
+++ JWeather/JWeather/Entities/SkyImages.swift
@@ -0,0 +1,19 @@
+//
+//  SkyImages.swift
+//  JWeather
+//
+//  Created by Jonah Pickett on 10/3/24.
+//
+
+enum SkyImages: String {
+    case clear = "clear-sky"
+    case cloudy = "cloudy-sky"
+    case evening = "evening-sky"
+    case morning = "morning-sky"
+    case night = "night-sky"
+    case rain = "rainy-sky"
+    case snow = "snowy-sky"
+    case sunny = "sunny-sky"
+    case mist = "misty-sky"
+    case thunderstorm = "thunderstorm-sky"
+}
