00669032d43be69f61ef815774ccdfc4398eed49
Jonah Pickett <jpickettpre@gmail.com>
2024-10-03 19:25:07 -0400

    Created WeatherContentView as module for WeatherView containing current weather data.

---
+++ JWeather/JWeather/Views/CurrentWeatherContentView.swift
@@ -0,0 +1,44 @@
+//
+//  CurrentWeatherContentView.swift
+//  JWeather
+//
+//  Created by Jonah Pickett on 10/3/24.
+//
+
+import SwiftUI
+
+struct CurrentWeatherContentView: View {
+    var body: some View {
+        VStack{
+            //MARK: Location
+            Text("My Location")
+                .foregroundStyle(.white)
+                .font(.title2)
+                .shadow(color: .black, radius: 5, x: 3, y: 0)
+            
+            // MARK: Temperature
+            Text("78°")
+                .foregroundStyle(.white)
+                .font(.system(size: 60))
+                .offset(x: 10)
+                .shadow(color: .black, radius: 5, x: 3, y: 0)
+            
+            // MARK: Sky
+            Text("Sunny")
+                .foregroundStyle(.white)
+                .font(.title3)
+                .shadow(color: .black, radius: 5, x: 3, y: 0)
+            
+            // MARK: High/Low
+            Text("H:80°\tL:70°")
+                .foregroundStyle(.white)
+                .shadow(color: .black, radius: 5, x: 3, y: 0)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CurrentWeatherContentView()
+}
